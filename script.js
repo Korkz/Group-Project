45bd0479321c81c88f787ee871b5ccd17c323ace
Korkz <77691313+Korkz@users.noreply.github.com>
2021-02-22 09:46:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-$("#Intro").click(function(){
-  $("#IntroP").toggle();
-});
-$("#Instructions").click(function(){
-  $("#InstructionsP").toggle();
-});
+$(document).ready(function(){
+  $("#Intro").click(function(){
+    $("#IntroP").toggle();
+  });
+  $("#Instructions").click(function(){
+    $("#InstructionsP").toggle();
+  });
 
-$("#Intro").mouseenter(function(){
-  $("#Intro").css('color':'blue');
-});
-$("#Intro").mouseleave(function(){
-  $("#Intro").css('color':'black');
-});
+  $("#Intro").mouseenter(function(){
+    $("#Intro").css('color':'blue');
+  });
+  $("#Intro").mouseleave(function(){
+    $("#Intro").css('color':'black');
+  });
 
-$("#Instructions").mouseenter(function(){
-  $("#Intro").css('color':'blue');
-});
-$("#Instructions").mouseleave(function(){
-  $("#Intro").css('color':'black');
+  $("#Instructions").mouseenter(function(){
+    $("#Intro").css('color':'blue');
+  });
+  $("#Instructions").mouseleave(function(){
+    $("#Intro").css('color':'black');
+  });
 });
